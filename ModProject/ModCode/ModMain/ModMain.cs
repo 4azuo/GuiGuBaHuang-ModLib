4b62967e7ff6d842b327393a33c3f8397352155b
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2024-02-15 01:25:35 +0900

    mpCost↑ coolDown↓

--- ModProject/ModCode/ModMain/ModMain.cs
+++ ModProject/ModCode/ModMain/ModMain.cs
@@ -1,4 +1,5 @@
 ﻿using ModLib.Mod;
+using System.Linq;
 
 namespace MOD_JhUKQ7
 {
@@ -24,6 +25,34 @@ namespace MOD_JhUKQ7
                 item.grade8 = (int)(item.grade8 * 3.5f);
                 item.grade9 = (int)(item.grade9 * 4.0f);
                 item.grade10 = (int)(item.grade10 * 5.0f);
+            }
+            //skill need more mpCost
+            foreach (var item in g.conf.battleSkillValue._allConfList.ToArray().Where(x => x.key.EndsWith("_mpCost")))
+            {
+                item.value1 = (item.value1.Parse<float>() * 1.5f).Parse<int>().ToString();
+                item.value2 = (item.value2.Parse<float>() * 1.6f).Parse<int>().ToString();
+                item.value3 = (item.value3.Parse<float>() * 1.8f).Parse<int>().ToString();
+                item.value4 = (item.value4.Parse<float>() * 2.0f).Parse<int>().ToString();
+                item.value5 = (item.value5.Parse<float>() * 2.3f).Parse<int>().ToString();
+                item.value6 = (item.value6.Parse<float>() * 2.6f).Parse<int>().ToString();
+                item.value7 = (item.value7.Parse<float>() * 3.0f).Parse<int>().ToString();
+                item.value8 = (item.value8.Parse<float>() * 3.5f).Parse<int>().ToString();
+                item.value9 = (item.value9.Parse<float>() * 4.0f).Parse<int>().ToString();
+                item.value10 = (item.value10.Parse<float>() * 5.0f).Parse<int>().ToString();
+            }
+            //skill need more mpCost
+            foreach (var item in g.conf.battleSkillValue._allConfList.ToArray().Where(x => x.key.EndsWith("_cd")))
+            {
+                item.value1 = (item.value1.Parse<float>() * 0.70f).Parse<int>().ToString();
+                item.value2 = (item.value2.Parse<float>() * 0.72f).Parse<int>().ToString();
+                item.value3 = (item.value3.Parse<float>() * 0.74f).Parse<int>().ToString();
+                item.value4 = (item.value4.Parse<float>() * 0.76f).Parse<int>().ToString();
+                item.value5 = (item.value5.Parse<float>() * 0.78f).Parse<int>().ToString();
+                item.value6 = (item.value6.Parse<float>() * 0.80f).Parse<int>().ToString();
+                item.value7 = (item.value7.Parse<float>() * 0.82f).Parse<int>().ToString();
+                item.value8 = (item.value8.Parse<float>() * 0.84f).Parse<int>().ToString();
+                item.value9 = (item.value9.Parse<float>() * 0.86f).Parse<int>().ToString();
+                item.value10 = (item.value10.Parse<float>() * 0.88f).Parse<int>().ToString();
             }
         }
     }
