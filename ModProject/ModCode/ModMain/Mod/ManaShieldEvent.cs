2bea34567b3680d4189bc9b61b6503d6d6e40e8e
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2024-02-17 23:11:52 +0900

    add manashield: TESTING

---
+++ ModProject/ModCode/ModMain/Mod/ManaShieldEvent.cs
@@ -0,0 +1,27 @@
+﻿using EBattleTypeData;
+using Harmony;
+using MOD_nE7UL2.Const;
+using ModLib.Enum;
+using ModLib.Mod;
+
+namespace MOD_nE7UL2.Mod
+{
+    //TESTING
+    //[Cache(ModConst.MANA_SHIELD_EVENT)]
+    //public sealed class ManaShieldEvent : ModEvent
+    //{
+    //    public override void OnBattleSetUnitType(SetUnitType e)
+    //    {
+    //        var humanData = e?.unit?.data?.TryCast<UnitDataHuman>();
+    //        if (humanData != null)
+    //        {
+    //            humanData.AddHP(new MartialTool.HitData(humanData.unit, null, 0, 0, /*humanData.worldUnitData.unit.GetDynProperty(UnitDynPropertyEnum.Mp).value / 10*/9999)
+    //            {
+    //                isShieldDef = true,
+    //            });
+    //            DebugHelper.WriteLine($"TEST");
+    //        }
+    //        DebugHelper.Save();
+    //    }
+    //}
+}
