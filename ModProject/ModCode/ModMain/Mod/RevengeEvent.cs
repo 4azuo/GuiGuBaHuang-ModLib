60834026cb215c1789a1622e551770bde04034e8
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2024-02-20 14:04:53 +0900

    add RevengeEvent

---
+++ ModProject/ModCode/ModMain/Mod/RevengeEvent.cs
@@ -0,0 +1,38 @@
+﻿using EBattleTypeData;
+using MOD_nE7UL2.Const;
+using ModLib.Mod;
+using System;
+
+namespace MOD_nE7UL2.Mod
+{
+    [Cache(ModConst.REVENGE_EVENT)]
+    public class RevengeEvent : ModEvent
+    {
+        public int Counter { get; set; } = 0;
+
+        //player kill npc
+        public override void OnBattleUnitDie(UnitDie e)
+        {
+            var dieUnit = e?.unit?.data?.TryCast<UnitDataHuman>();
+            var attackUnitData = e?.hitData?.attackUnit?.data?.TryCast<UnitDataHuman>();
+            if (dieUnit?.worldUnitData != null && 
+                g.world.battle.data.isRealBattle &&
+                (attackUnitData?.worldUnitData?.unit?.IsPlayer() ?? false))
+            {
+                Counter++;
+            }
+        }
+
+        //npc revenge player
+        public override void OnIntoBattleFirst(UnitCtrlBase e)
+        {
+            var humanData = e?.data?.TryCast<UnitDataHuman>();
+            if (g.world.battle.data.isRealBattle &&
+                (!humanData?.worldUnitData?.unit?.IsPlayer() ?? false) &&
+                humanData?.worldUnitData?.GetRelationType(g.world.playerUnit) == UnitBothRelationType.Hater)
+            {
+                humanData.attack.baseValue += (int)(humanData.attack.baseValue * (Math.Sqrt(Counter) / 100.00f));
+            }
+        }
+    }
+}
