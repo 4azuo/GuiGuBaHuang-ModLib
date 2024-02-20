c1465033a86b462e82d697a0c15f5b075ab1f692
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2024-02-20 14:00:41 +0900

    list all Effect

---
+++ ModProject/ModCode/ModLib/Helper/EffectHelper.cs
@@ -0,0 +1,24 @@
+﻿using ModLib.Object;
+using System;
+using System.Reflection;
+
+//Effect3017: Shield
+public static class EffectHelper
+{
+    public static void ShowStaticMethods()
+    {
+        var ass = Assembly.LoadFile("C:\\Program Files (x86)\\Steam\\steamapps\\common\\鬼谷八荒\\MelonLoader\\Managed\\Assembly-CSharp.dll");
+        for (int i = 0; i < 10000; i++)
+        {
+            var t = ass.GetType($"Effect{i:0000}");
+            if ( t != null)
+            {
+                DebugHelper.WriteLine($"{t.Name}");
+                foreach (var m in t.GetMethods(BindingFlags.Static | BindingFlags.Public))
+                {
+                    DebugHelper.WriteLine($"　{m.Name}");
+                }
+            }
+        }
+    }
+}
