a0baf8a149716fc0e11422c53f128dbe31f06feb
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2024-02-28 22:51:05 +0900

    disable Debug

--- ModProject/ModCode/ModMain/Mod/DebugEvent.cs
+++ ModProject/ModCode/ModMain/Mod/DebugEvent.cs
@@ -1,22 +1,22 @@
-﻿using ModLib.Enum;
-using ModLib.Mod;
+﻿//using ModLib.Enum;
+//using ModLib.Mod;
 
-namespace MOD_nE7UL2.Mod
-{
-    [Cache("DEBUG")]
-    public class DebugEvent : ModEvent
-    {
-        public override void OnLoadGame()
-        {
-            g.world.playerUnit.AddProperty<int>(UnitPropertyEnum.Attack, 1000000);
-            g.world.playerUnit.AddProperty<int>(UnitPropertyEnum.Defense, 1000000);
-            g.world.playerUnit.AddProperty<int>(UnitPropertyEnum.HpMax, 1000000);
-            g.world.playerUnit.AddProperty<int>(UnitPropertyEnum.MpMax, 1000000);
-            g.world.playerUnit.AddProperty<int>(UnitPropertyEnum.SpMax, 1000000);
-            g.world.playerUnit.data.RewardPropMoney(1000000);
+//namespace MOD_nE7UL2.Mod
+//{
+//    [Cache("DEBUG")]
+//    public class DebugEvent : ModEvent
+//    {
+//        public override void OnLoadGame()
+//        {
+//            g.world.playerUnit.AddProperty<int>(UnitPropertyEnum.Attack, 1000000);
+//            g.world.playerUnit.AddProperty<int>(UnitPropertyEnum.Defense, 1000000);
+//            g.world.playerUnit.AddProperty<int>(UnitPropertyEnum.HpMax, 1000000);
+//            g.world.playerUnit.AddProperty<int>(UnitPropertyEnum.MpMax, 1000000);
+//            g.world.playerUnit.AddProperty<int>(UnitPropertyEnum.SpMax, 1000000);
+//            g.world.playerUnit.data.RewardPropMoney(1000000);
 
-            g.world.playerUnit.data.unitData.propertyData.footSpeed = 10000;
-            g.world.playerUnit.data.dynUnitData.playerView.baseValue = 20;
-        }
-    }
-}
+//            g.world.playerUnit.data.unitData.propertyData.footSpeed = 10000;
+//            g.world.playerUnit.data.dynUnitData.playerView.baseValue = 20;
+//        }
+//    }
+//}
