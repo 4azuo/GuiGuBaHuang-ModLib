d2f2df105dbd4384b5ad5f012c0452e19a45cd46
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2024-03-11 01:43:17 +0900

    update

--- ModProject/ModCode/ModMain/Mod/DebugEvent.cs
+++ ModProject/ModCode/ModMain/Mod/DebugEvent.cs
@@ -1,8 +1,11 @@
 ﻿//using EGameTypeData;
+//using ModLib.Enum;
 //using ModLib.Mod;
 //using System.Linq;
+//using System.Reflection;
 //using TMPro;
 //using UnityEngine;
+//using UnityEngine.Events;
 //using UnityEngine.EventSystems;
 //using UnityEngine.UI;
 
@@ -11,6 +14,7 @@
 //    [Cache("DEBUG", IsGlobal = true)]
 //    public class DebugEvent : ModEvent
 //    {
+//        /* === In game debug === */
 //        //public override void OnLoadGame()
 //        //{
 //        //    g.world.playerUnit.AddProperty<int>(UnitPropertyEnum.Attack, 1000000);
@@ -21,9 +25,10 @@
 //        //    g.world.playerUnit.data.RewardPropMoney(1000000);
 
 //        //    g.world.playerUnit.data.unitData.propertyData.footSpeed = 10000;
-//        //    g.world.playerUnit.data.dynUnitData.playerView.baseValue = 20;
+//        //    g.world.playerUnit.data.dynUnitData.playerView.baseValue = 1000;
 //        //}
 
+//        /* === Ui component list === */
 //        //public override void OnOpenUIStart(OpenUIStart e)
 //        //{
 //        //    DebugHelper.WriteLine($"UI: {e.uiType.uiName}");
@@ -45,6 +50,27 @@
 //        //        DebugHelper.WriteLine();
 //        //    }
 //        //    DebugHelper.Save();
+//        //}
+
+//        /* === Button insert action === */
+//        //bool test = true;
+//        //public override void OnOpenUIEnd(OpenUIEnd e)
+//        //{
+//        //    var ui = MonoBehaviour.FindObjectOfType<UIStartGameTip>();
+//        //    if (ui != null)
+//        //    {
+//        //        if (test)
+//        //        {
+//        //            ui.btnOK.onClick.m_Calls.m_RuntimeCalls.Insert(0, new InvokableCall((UnityAction)xxx));
+//        //            test = false;
+//        //        }
+//        //        DebugHelper.WriteLine($"{ui.btnOK.onClick.m_Calls.m_RuntimeCalls.Count}");
+//        //        DebugHelper.Save();
+//        //    }
+//        //}
+//        //private void xxx()
+//        //{
+//        //    DebugHelper.WriteLine($"TEST");
 //        //}
 //    }
 //}
