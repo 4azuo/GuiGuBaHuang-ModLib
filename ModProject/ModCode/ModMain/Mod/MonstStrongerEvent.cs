f77df046c0c3d70f1509c9323b563572544e176a
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2024-03-11 01:43:06 +0900

    monster stronger

--- ModProject/ModCode/ModMain/Mod/MonstStrongerEvent.cs
+++ ModProject/ModCode/ModMain/Mod/MonstStrongerEvent.cs
@@ -1,4 +1,5 @@
-﻿using MOD_nE7UL2.Const;
+﻿using EBattleTypeData;
+using MOD_nE7UL2.Const;
 using ModLib.Mod;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,6 +16,18 @@ namespace MOD_nE7UL2.Mod
             [MonstType.BOSS] = 0.0005f,
         };
 
+        public static readonly IDictionary<MonstType, float> KILL_GROW_RATIO = new Dictionary<MonstType, float>
+        {
+            [MonstType.Elite] = 0.00004f,
+            [MonstType.BOSS] = 0.0001f,
+        };
+
+        public IDictionary<MonstType, int> KillCounter = new Dictionary<MonstType, int>
+        {
+            [MonstType.Elite] = 0,
+            [MonstType.BOSS] = 0,
+        };
+
         public int Counter { get; set; } = 0;
 
         public override void OnMonthly()
@@ -22,12 +35,21 @@ namespace MOD_nE7UL2.Mod
             Counter++;
         }
 
+        public override void OnBattleUnitDie(UnitDie e)
+        {
+            var monstData = e?.unit?.data?.TryCast<UnitDataMonst>();
+            if (monstData != null && KILL_GROW_RATIO.ContainsKey(monstData.monstType))
+            {
+                KillCounter[monstData.monstType]++;
+            }
+        }
+
         public override void OnIntoBattleFirst(UnitCtrlBase e)
         {
             var monstData = e?.data?.TryCast<UnitDataMonst>();
             if (monstData != null && GROW_RATIO.ContainsKey(monstData.monstType))
             {
-                var ratio = Counter * GROW_RATIO[monstData.monstType];
+                var ratio = (Counter * GROW_RATIO[monstData.monstType]) + (KillCounter[monstData.monstType] * KILL_GROW_RATIO[monstData.monstType]);
                 monstData.attack.baseValue += (int)(monstData.attack.baseValue * ratio);
                 monstData.maxHP.baseValue += (int)(monstData.attack.baseValue * ratio);
             }
