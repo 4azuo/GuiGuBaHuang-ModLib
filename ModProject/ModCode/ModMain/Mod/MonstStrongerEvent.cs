ae7d23944eed4b8110496b7a1eec7bc52f775bf3
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2024-04-17 21:46:19 +0700

    2.1.2
    fix bugs

--- ModProject/ModCode/ModMain/Mod/MonstStrongerEvent.cs
+++ ModProject/ModCode/ModMain/Mod/MonstStrongerEvent.cs
@@ -18,12 +18,14 @@ namespace MOD_nE7UL2.Mod
 
         public static readonly IDictionary<MonstType, float> KILL_GROW_RATIO = new Dictionary<MonstType, float>
         {
+            [MonstType.Common] = 0,
             [MonstType.Elite] = 0.00004f,
             [MonstType.BOSS] = 0.0001f,
         };
 
         public IDictionary<MonstType, int> KillCounter = new Dictionary<MonstType, int>
         {
+            [MonstType.Common] = 0,
             [MonstType.Elite] = 0,
             [MonstType.BOSS] = 0,
         };
