545483ec9a7ceba4d11969b7e3a937ac734c2b42
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2024-09-22 20:41:43 +0700

    2.3.26

--- ModProject/ModCode/ModMain/ModMain.cs
+++ ModProject/ModCode/ModMain/ModMain.cs
@@ -8,7 +8,7 @@ using System.Linq;
 
 namespace MOD_nE7UL2
 {
-    [InGameCustomSettings("game_configs.json", "2024/09/22")]
+    [InGameCustomSettings("game_configs.json", "2024/09/22(2)")]
     public sealed class ModMain : ModMaster<InGameStts>
     {
         public override string ModName => "MOD_nE7UL2";
