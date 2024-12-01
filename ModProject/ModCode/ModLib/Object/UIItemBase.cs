f105eebee7769d8131f2f32144d28bc2b311fc86
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2024-12-02 01:49:38 +0700

    3.4.3

--- ModProject/ModCode/ModLib/Object/UIItemBase.cs
+++ ModProject/ModCode/ModLib/Object/UIItemBase.cs
@@ -6,11 +6,11 @@ using UnityEngine.UI;
 
 public abstract class UIItemBase
 {
+    public UIItemBase Parent { get; private set; }
     public virtual UIBehaviour ItemBehaviour { get; private set; }
     public virtual UIItemData ItemData { get; set; }
     public virtual UIItemWork ItemWork { get; set; }
     public virtual object Tag { get; set; }
-    public virtual object PrevValue { get; internal set; }
 
     public virtual void Update() { }
     public abstract object Get();
@@ -45,7 +45,7 @@ public abstract class UIItemBase
     {
         public virtual Func<UIItemBase, bool> EnaAct { get; set; }
         public virtual Func<UIItemBase, object[]> Formatter { get; set; }
-        public virtual Action<UIItemBase> Change { get; set; }
+        public virtual Action<UIItemBase, object> Change { get; set; }
     }
     #endregion
 
@@ -74,18 +74,12 @@ public abstract class UIItemBase
         {
             MonoBehaviour.Destroy(Item);
         }
-
-        public override void Update()
-        {
-            base.Update();
-            if (ItemWork?.EnaAct != null)
-                Item.enabled = ItemWork?.EnaAct?.Invoke(this) ?? false;
-        }
     }
 
     public class UIItemText : UIItem<Text>
     {
         public string FormatStr { get; private set; }
+        public Color Color { get; private set; }
 
         public UIItemText(Transform t, float x, float y, string format) : base(UIHelper._sampleUI.textSystemOK.Create(t).Pos(UIHelper._originComp.gameObject, x, y))
         {
@@ -93,6 +87,7 @@ public abstract class UIItemBase
             Item.Setup(Get()?.ToString());
             Item.Align(TextAnchor.MiddleCenter);
             Item.Format();
+            Color = Item.color;
         }
 
         public override object Get()
@@ -114,6 +109,8 @@ public abstract class UIItemBase
         {
             base.Update();
             Item.Setup(Get()?.ToString());
+            if (ItemWork?.EnaAct != null)
+                Item.color = (ItemWork?.EnaAct?.Invoke(this) ?? false) ? Color : Color.gray;
         }
 
         public UIItemText Align(TextAnchor tanchor = TextAnchor.MiddleLeft, VerticalWrapMode vMode = VerticalWrapMode.Overflow, HorizontalWrapMode hMode = HorizontalWrapMode.Overflow)
@@ -125,6 +122,7 @@ public abstract class UIItemBase
         public UIItemText Format(Color? color = null, int fsize = 15, FontStyle fstype = FontStyle.Normal)
         {
             Item.Format(color, fsize, fstype);
+            Color = Item.color;
             return this;
         }
     }
@@ -141,8 +139,7 @@ public abstract class UIItemBase
             Item.Setup(min, max, def);
             Item.onValueChanged.AddListener((UnityAction<float>)(v =>
             {
-                this.PrevValue = v;
-                ItemWork?.Change?.Invoke(this);
+                ItemWork?.Change?.Invoke(this, v);
             }));
         }
 
@@ -155,6 +152,20 @@ public abstract class UIItemBase
         {
             Item.Setup(Min, Max, (float)input);
         }
+
+        public void SetPercent(float percent, float min = float.MinValue, float max = float.MaxValue)
+        {
+            min = min == float.MinValue ? Min : min;
+            max = max == float.MaxValue ? Max : max;
+            Set(((max - min) * percent) + min);
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            if (ItemWork?.EnaAct != null)
+                Item.enabled = ItemWork?.EnaAct?.Invoke(this) ?? false;
+        }
     }
 
     public class UIItemToggle : UIItem<Toggle>
@@ -164,8 +175,7 @@ public abstract class UIItemBase
             Item.Setup(def);
             Item.onValueChanged.AddListener((UnityAction<bool>)(v =>
             {
-                this.PrevValue = v;
-                ItemWork?.Change?.Invoke(this);
+                ItemWork?.Change?.Invoke(this, v);
             }));
         }
 
@@ -177,6 +187,13 @@ public abstract class UIItemBase
         public override void Set(object input)
         {
             Item.Setup((bool)input);
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            if (ItemWork?.EnaAct != null)
+                Item.enabled = ItemWork?.EnaAct?.Invoke(this) ?? false;
         }
     }
 
@@ -220,7 +237,7 @@ public abstract class UIItemBase
         public UIItemText Prefix { get; private set; }
         public UIItemBase MainComponent { get; private set; }
         public UIItemText Postfix { get; private set; }
-        public bool HidePostfixIfDisabled { get; set; }
+        public bool HidePostfixIfDisabled { get; set; } = false;
 
         public override UIBehaviour ItemBehaviour => MainComponent.ItemBehaviour;
         public override UIItemData ItemData
@@ -244,41 +261,34 @@ public abstract class UIItemBase
             }
         }
         public override object Tag { get => MainComponent.Tag; set => MainComponent.Tag = value; }
-        public override object PrevValue { get => MainComponent.PrevValue; internal set => MainComponent.PrevValue = value; }
 
         private UIItemComposite() { }
 
         public static UIItemComposite CreateSlider(Transform t, float x, float y, string prefix, float min, float max, float def, string postfix = null)
         {
             var rs = new UIItemComposite();
-            if (prefix != null)
-            {
-                rs.Prefix = new UIItemText(t, x, y, prefix);
-                rs.Prefix.Item.Align(TextAnchor.MiddleRight);
-            }
+            rs.Prefix = new UIItemText(t, x, y, prefix);
+            rs.Prefix.Item.Align(TextAnchor.MiddleRight);
+            rs.Prefix.Parent = rs;
             rs.MainComponent = new UIItemSlider(t, x + UIHelper.DELTA_X * 4, y, min, max, def);
-            if (postfix != null)
-            {
-                rs.Postfix = new UIItemText(t, x + UIHelper.DELTA_X * 8, y, postfix);
-                rs.Postfix.Item.Align(TextAnchor.MiddleLeft);
-            }
+            rs.MainComponent.Parent = rs;
+            rs.Postfix = new UIItemText(t, x + UIHelper.DELTA_X * 8, y, postfix);
+            rs.Postfix.Item.Align(TextAnchor.MiddleLeft);
+            rs.Postfix.Parent = rs;
             return rs;
         }
 
         public static UIItemComposite CreateToggle(Transform t, float x, float y, string prefix, bool def, string postfix = null)
         {
             var rs = new UIItemComposite();
-            if (prefix != null)
-            {
-                rs.Prefix = new UIItemText(t, x, y, prefix);
-                rs.Prefix.Item.Align(TextAnchor.MiddleRight);
-            }
+            rs.Prefix = new UIItemText(t, x, y, prefix);
+            rs.Prefix.Item.Align(TextAnchor.MiddleRight);
+            rs.Prefix.Parent = rs;
             rs.MainComponent = new UIItemToggle(t, x + UIHelper.DELTA_X * 2, y, def);
-            if (postfix != null)
-            {
-                rs.Postfix = new UIItemText(t, x + UIHelper.DELTA_X * 4, y, postfix);
-                rs.Postfix.Item.Align(TextAnchor.MiddleLeft);
-            }
+            rs.MainComponent.Parent = rs;
+            rs.Postfix = new UIItemText(t, x + UIHelper.DELTA_X * 4, y, postfix);
+            rs.Postfix.Item.Align(TextAnchor.MiddleLeft);
+            rs.Postfix.Parent = rs;
             return rs;
         }
 
--- ModProject/ModCode/ModMain/Mod/SMConfigEvent.cs
+++ ModProject/ModCode/ModMain/Mod/SMConfigEvent.cs
@@ -63,14 +63,13 @@ namespace MOD_nE7UL2.Mod
         private UIItemBase.UIItemComposite tglSysNoExpFromBattle;
 
         //Score
-        private readonly IList<SMItem> ScoreCalculator = new List<SMItem>();
+        public static IList<SMItemWork> ScoreCalculator { get; } = new List<SMItemWork>();
 
         public override void OnLoadGlobal()
         {
             base.OnLoadGlobal();
 
-            DebugHelper.WriteLine("1");
-            Register(() => txtTotalScore, s => CalSMTotalScore());
+            ScoreCalculator.Clear();
             Register(() => slMonstAtk, s => (s.Get().Parse<float>() * 100).Parse<int>());
             Register(() => slMonstDef, s => (s.Get().Parse<float>() * 100).Parse<int>());
             Register(() => slMonstHp, s => (s.Get().Parse<float>() * 100).Parse<int>());
@@ -83,15 +82,13 @@ namespace MOD_nE7UL2.Mod
             Register(() => slEcoBankFee, s => (s.Get().Parse<float>() * 100).Parse<int>());
             Register(() => slNpcGrowRate, s => (s.Get().Parse<float>() * 1000).Parse<int>());
             Register(() => slMiscLevelupExp, s => (s.Get().Parse<float>() * 2000).Parse<int>());
-            Register(() => tglSysHideSave, s => 1000, s => s.Get().Parse<bool>()/*, onChange: s => tglSysHideReload.Set(false)*/);
-            Register(() => tglSysHideReload, s => 5000, s => s.Get().Parse<bool>()/*, s => tglSysHideSave.Get().Parse<bool>()*//*, onChange: s => tglSysOnelife.Set(false)*/);
+            Register(() => tglSysHideSave, s => 1000, s => s.Get().Parse<bool>(), onChange: (s, v) => tglSysHideReload.Set(false));
+            Register(() => tglSysHideReload, s => 5000, s => s.Get().Parse<bool>(), s => tglSysHideSave.Get().Parse<bool>(), onChange: (s, v) => tglSysOnelife.Set(false));
             Register(() => tglSysHideBattleMap, s => 2000, s => s.Get().Parse<bool>());
             Register(() => tglSysNoRebirth, s => 10000, s => s.Get().Parse<bool>());
-            Register(() => tglSysOnelife, s => 20000, s => s.Get().Parse<bool>()/*, s => tglSysHideReload.Get().Parse<bool>()*/);
+            Register(() => tglSysOnelife, s => 20000, s => s.Get().Parse<bool>(), s => tglSysHideReload.Get().Parse<bool>());
             Register(() => tglSysOnlyPortalAtCityAndSect, s => 1000, s => s.Get().Parse<bool>());
             Register(() => tglSysNoExpFromBattle, s => 1000, s => s.Get().Parse<bool>());
-            DebugHelper.WriteLine(ScoreCalculator.Count.ToString());
-            DebugHelper.Save();
         }
 
         private void Register(
@@ -100,10 +97,25 @@ namespace MOD_nE7UL2.Mod
             Func<UIItemBase, bool> funcCond = null,
             Func<UIItemBase, bool> funcEna = null,
             Func<UIItemBase, object[]> funcFormatter = null,
-            Action<UIItemBase> onChange = null)
+            Action<UIItemBase, object> onChange = null)
         {
-            var formatter = funcFormatter ?? (s => new object[] { funcCal.Invoke(s), s.Get().Parse<float>() });
-            ScoreCalculator.Add(new SMItem
+            var formatter = funcFormatter ?? (s =>
+            {
+                var rs = new object[] { 0, 0 };
+                if (s.Parent != null)
+                {
+                    //point
+                    rs[0] = CalCompScore(s.Parent);
+                    //%
+                    var x = s.Parent as UIItemBase.UIItemComposite;
+                    if (x.MainComponent is UIItemBase.UIItemSlider)
+                    {
+                        rs[1] = (x.Get().Parse<float>() * 100).Parse<int>().ToString("+#;-#;0");
+                    }
+                }
+                return rs;
+            });
+            ScoreCalculator.Add(new SMItemWork
             {
                 Comp = funcComp,
                 Cal = funcCal,
@@ -133,32 +145,32 @@ namespace MOD_nE7UL2.Mod
             int col, row;
 
             col = 1; row = 0;
-            uiCustom.AddText(col, row++, "Monster:").Format(null, 17, FontStyle.Italic);
-            slMonstAtk = uiCustom.AddCompositeSlider(col, row++, "ATK", -10.00f, 10.00f, AddAtkRate, "+{1}% ({0}P)");
-            slMonstDef = uiCustom.AddCompositeSlider(col, row++, "DEF", -10.00f, 10.00f, AddDefRate, "+{1}% ({0}P)");
-            slMonstHp = uiCustom.AddCompositeSlider(col, row++, "Max HP", -10.00f, 10.00f, AddHpRate, "+{1}% ({0}P)");
-            slMonstBasis = uiCustom.AddCompositeSlider(col, row++, "Basis", -10.00f, 10.00f, AddBasisRate, "+{1}% ({0}P)");
+            uiCustom.AddText(col, row++, "Monster:").Format(null, 17, FontStyle.Italic).Align(TextAnchor.MiddleRight);
+            slMonstAtk = uiCustom.AddCompositeSlider(col, row++, "ATK", -0.50f, 10.00f, AddAtkRate, "{1}% ({0}P)");
+            slMonstDef = uiCustom.AddCompositeSlider(col, row++, "DEF", -0.50f, 10.00f, AddDefRate, "{1}% ({0}P)");
+            slMonstHp = uiCustom.AddCompositeSlider(col, row++, "Max HP", -0.50f, 10.00f, AddHpRate, "{1}% ({0}P)");
+            slMonstBasis = uiCustom.AddCompositeSlider(col, row++, "Basis", -0.50f, 10.00f, AddBasisRate, "{1}% ({0}P)");
             uiCustom.AddText(col, row++, "(Included Sword, Blade, Spear, Fist, Finger, Palm, Fire, Water, Thunder, Wood, Wind, Earth)").Format(null, 13).Align(TextAnchor.MiddleLeft);
-            slMonstSpecialRate = uiCustom.AddCompositeSlider(col, row++, "Special Monster Rate", -1.00f, 1.00f, AddSpecialMonsterRate, "+{1}% ({0}P)");
+            slMonstSpecialRate = uiCustom.AddCompositeSlider(col, row++, "Special Monster Rate", -0.50f, 1.00f, AddSpecialMonsterRate, "{1}% ({0}P)");
 
             col = 1; row = 8;
-            uiCustom.AddText(col, row++, "Economic:").Format(null, 17, FontStyle.Italic);
-            slEcoTaxRate = uiCustom.AddCompositeSlider(col, row++, "Tax Rate", -10.00f, 10.00f, AddTaxRate, "+{1}% ({0}P)");
-            slEcoInfRate = uiCustom.AddCompositeSlider(col, row++, "Inflation Rate", -10.00f, 10.00f, AddInflationRate, "+{1}% ({0}P)");
-            slEcoBuildingCost = uiCustom.AddCompositeSlider(col, row++, "Building Cost", -10.00f, 10.00f, AddBuildingCostRate, "+{1}% ({0}P)");
-            slEcoBankAccCost = uiCustom.AddCompositeSlider(col, row++, "Bank Account Cost", -10.00f, 10.00f, AddBankAccountCostRate, "+{1}% ({0}P)");
-            slEcoBankFee = uiCustom.AddCompositeSlider(col, row++, "Bank Fee", -10.00f, 10.00f, AddBankFee, "+{1}% ({0}P)");
+            uiCustom.AddText(col, row++, "Economic:").Format(null, 17, FontStyle.Italic).Align(TextAnchor.MiddleRight);
+            slEcoTaxRate = uiCustom.AddCompositeSlider(col, row++, "Tax Rate", 0.00f, 10.00f, AddTaxRate, "{1}% ({0}P)");
+            slEcoInfRate = uiCustom.AddCompositeSlider(col, row++, "Inflation Rate", -0.50f, 3.00f, AddInflationRate, "{1}% ({0}P)");
+            slEcoBuildingCost = uiCustom.AddCompositeSlider(col, row++, "Building Cost", 0.00f, 10.00f, AddBuildingCostRate, "{1}% ({0}P)");
+            slEcoBankAccCost = uiCustom.AddCompositeSlider(col, row++, "Bank Account Cost", 0.00f, 10.00f, AddBankAccountCostRate, "{1}% ({0}P)");
+            slEcoBankFee = uiCustom.AddCompositeSlider(col, row++, "Bank Fee", 0.00f, 100.00f, AddBankFee, "{1}% ({0}P)");
 
             col = 1; row = 15;
-            uiCustom.AddText(col, row++, "NPC:").Format(null, 17, FontStyle.Italic);
-            slNpcGrowRate = uiCustom.AddCompositeSlider(col, row++, "Grow Rate", -10.00f, 10.00f, AddNpcGrowRate, "+{1}% ({0}P)");
+            uiCustom.AddText(col, row++, "NPC:").Format(null, 17, FontStyle.Italic).Align(TextAnchor.MiddleRight);
+            slNpcGrowRate = uiCustom.AddCompositeSlider(col, row++, "Grow Rate", 0.00f, 10.00f, AddNpcGrowRate, "{1}% ({0}P)");
 
             col = 1; row = 18;
-            uiCustom.AddText(col, row++, "Misc:").Format(null, 17, FontStyle.Italic);
-            slMiscLevelupExp = uiCustom.AddCompositeSlider(col, row++, "Levelup Exp", -10.00f, 10.00f, AddLevelupExpRate, "+{1}% ({0}P)");
+            uiCustom.AddText(col, row++, "Misc:").Format(null, 17, FontStyle.Italic).Align(TextAnchor.MiddleRight);
+            slMiscLevelupExp = uiCustom.AddCompositeSlider(col, row++, "Levelup Exp", 0.00f, 1.00f, AddLevelupExpRate, "{1}% ({0}P)");
 
             col = 16; row = 0;
-            uiCustom.AddText(col, row++, "Systems:").Format(null, 17, FontStyle.Italic);
+            uiCustom.AddText(col, row++, "Systems:").Format(null, 17, FontStyle.Italic).Align(TextAnchor.MiddleRight);
             tglSysHideSave = uiCustom.AddCompositeToggle(col, row++, "Hide Save Button", HideSaveButton, "({0}P)");
             tglSysHideReload = uiCustom.AddCompositeToggle(col, row++, "Hide Reload Button", HideReloadButton, "({0}P)");
             tglSysHideBattleMap = uiCustom.AddCompositeToggle(col, row++, "Hide Battle Map", HideBattleMap, "({0}P)");
@@ -168,28 +180,21 @@ namespace MOD_nE7UL2.Mod
             tglSysNoExpFromBattle = uiCustom.AddCompositeToggle(col, row++, "No Exp from Battles", NoExpFromBattles, "({0}P)");
 
             col = 30; row = 0;
-            txtTotalScore = uiCustom.AddText(col, 0, "Total score: {0}P").Format(Color.red, 17).Align(TextAnchor.MiddleRight);
-            uiCustom.AddButton(col, 2, () => { }, "Level 1");
-            uiCustom.AddButton(col, 4, () => { }, "Level 2");
-            uiCustom.AddButton(col, 6, () => { }, "Level 3");
-            uiCustom.AddButton(col, 8, () => { }, "Level 4");
-            uiCustom.AddButton(col, 10, () => { }, "Level 5");
-            uiCustom.AddButton(col, 12, () => { }, "Level 6");
-            uiCustom.AddButton(col, 14, () => { }, "Level 7");
-            uiCustom.AddButton(col, 16, () => { }, "Level 8");
-            uiCustom.AddButton(col, 18, () => { }, "Level 9");
-            uiCustom.AddButton(col, 20, () => { }, "Level 10");
+            txtTotalScore = uiCustom.AddText(col, row, "Total score: {0}P").Format(Color.red, 17).Align(TextAnchor.MiddleRight);
+            uiCustom.AddButton(col, row += 2, () => SetLevel(0), "Default");
+            uiCustom.AddButton(col, row += 2, () => SetLevel(1), "Level 1");
+            uiCustom.AddButton(col, row += 2, () => SetLevel(2), "Level 2");
+            uiCustom.AddButton(col, row += 2, () => SetLevel(3), "Level 3");
+            uiCustom.AddButton(col, row += 2, () => SetLevel(4), "Level 4");
+            uiCustom.AddButton(col, row += 2, () => SetLevel(5), "Level 5");
+            uiCustom.AddButton(col, row += 2, () => SetLevel(6), "Level 6");
+            uiCustom.AddButton(col, row += 2, () => SetLevel(7), "Level 7");
+            uiCustom.AddButton(col, row += 2, () => SetLevel(8), "Level 8");
+            uiCustom.AddButton(col, row += 2, () => SetLevel(9), "Level 9");
+            uiCustom.AddButton(col, row += 2, () => SetLevel(10), "Level 10");
             uiCustom.AddText(14, 26, "You have to start a new game to apply these configs!").Format(Color.red, 17);
 
             SetWork();
-            try
-            {
-                txtTotalScore.Set($"Total score: {CalSMTotalScore()}P");
-            }
-            catch (Exception ex)
-            {
-                DebugHelper.WriteLine(ex);
-            }
         }
 
         private void SetWork()
@@ -201,12 +206,35 @@ namespace MOD_nE7UL2.Mod
             }
         }
 
+        private void SetLevel(int level)
+        {
+            (slMonstAtk.MainComponent as UIItemBase.UIItemSlider).SetPercent(level * 0.10000f);
+            (slMonstDef.MainComponent as UIItemBase.UIItemSlider).SetPercent(level * 0.10000f);
+            (slMonstHp.MainComponent as UIItemBase.UIItemSlider).SetPercent(level * 0.10000f, 0f);
+            (slMonstBasis.MainComponent as UIItemBase.UIItemSlider).SetPercent(level * 0.10000f);
+            (slMonstSpecialRate.MainComponent as UIItemBase.UIItemSlider).SetPercent(level * 0.10000f);
+            (slEcoTaxRate.MainComponent as UIItemBase.UIItemSlider).SetPercent(level * 0.10000f);
+            (slEcoInfRate.MainComponent as UIItemBase.UIItemSlider).SetPercent(level * 0.10000f);
+            (slEcoBuildingCost.MainComponent as UIItemBase.UIItemSlider).SetPercent(level * 0.10000f);
+            (slEcoBankAccCost.MainComponent as UIItemBase.UIItemSlider).SetPercent(level * 0.10000f);
+            (slEcoBankFee.MainComponent as UIItemBase.UIItemSlider).SetPercent(level * 0.10000f);
+            (slNpcGrowRate.MainComponent as UIItemBase.UIItemSlider).SetPercent(level * 0.10000f);
+            (slMiscLevelupExp.MainComponent as UIItemBase.UIItemSlider).SetPercent(level * 0.10000f);
+            tglSysHideBattleMap.Set(level > 1);
+            tglSysHideSave.Set(level > 2);
+            tglSysNoExpFromBattle.Set(level > 4);
+            tglSysOnlyPortalAtCityAndSect.Set(level > 5);
+            tglSysHideReload.Set(level > 7);
+            tglSysNoRebirth.Set(level > 8);
+            tglSysOnelife.Set(level > 9);
+        }
+
         [ErrorIgnore]
         public override void OnTimeUpdate()
         {
             base.OnTimeUpdate();
-            //uiCustom.UpdateUI();
-            //txtTotalScore.Set($"Total score: {CalSMTotalScore()}P");
+            uiCustom.UpdateUI();
+            txtTotalScore.Set($"Total score: {CalSMTotalScore()}P");
         }
 
         private void SetSMConfigs()
@@ -233,30 +261,34 @@ namespace MOD_nE7UL2.Mod
             CacheHelper.Save();
         }
 
-        private int CalCompScore(UIItemBase comp)
+        public static int CalCompScore(UIItemBase comp)
         {
-            if (comp == null)
-                return 0;
             var x = ScoreCalculator.FirstOrDefault(k => k.Comp.Invoke() == comp);
             if (x == null || !x.Cond.Invoke(comp))
                 return 0;
             return x.Cal.Invoke(comp);
         }
 
-        private bool IsEnableComp(UIItemBase comp)
+        public static bool IsEnableComp(UIItemBase comp)
         {
-            if (comp == null)
-                return false;
             var x = ScoreCalculator.FirstOrDefault(k => k.Comp.Invoke() == comp);
             if (x == null)
                 return false;
-            var item = x.Comp.Invoke();
-            return x != null && x.Cond.Invoke(comp) && item.IsEnable();
+            return x != null && x.Cond.Invoke(comp) && comp.IsEnable();
         }
 
-        private int CalSMTotalScore()
+        public static int CalSMTotalScore()
         {
-            return ScoreCalculator.Where(x => IsEnableComp(x.Comp.Invoke())).Sum(x => CalCompScore(x.Comp.Invoke()));
+            var rs = 0;
+            foreach (var item in ScoreCalculator)
+            {
+                var comp = item.Comp.Invoke();
+                if (comp != null && IsEnableComp(comp))
+                {
+                    rs += CalCompScore(comp);
+                }
+            }
+            return rs;
         }
     }
 }
