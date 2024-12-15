dcfd118d622112f8f975b78ed195485a0d5db538
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2024-12-16 03:19:16 +0700

    3.5.4e
    optimize

--- ModProject/ModCode/ModMain/Mod/ExplodeMonsterEvent.cs
+++ ModProject/ModCode/ModMain/Mod/ExplodeMonsterEvent.cs
@@ -1,5 +1,4 @@
-﻿using Boo.Lang;
-using EBattleTypeData;
+﻿using EBattleTypeData;
 using MOD_nE7UL2.Const;
 using ModLib.Mod;
 
@@ -11,48 +10,29 @@ namespace MOD_nE7UL2.Mod
         public const float MONST_EXPLODE_CHANCE = 0.50f;
         public const string EXPLODE_EFX = @"Effect\Battle\Skill\baiyuanshizhen";
 
-        private List<string> _ExMonst = new List<string>();
-
-        public override void OnBattleStart(ETypeData e)
-        {
-            base.OnBattleStart(e);
-            _ExMonst.Clear();
-        }
-
-        public override void OnBattleUnitInto(UnitCtrlBase e)
-        {
-            base.OnBattleUnitInto(e);
-
-            if (e.IsMonster())
-            {
-                var gameLvl = g.data.dataWorld.data.gameLevel.Parse<int>();
-                var monstData = e?.data?.TryCast<UnitDataMonst>();
-                var smConfigs = EventHelper.GetEvent<SMLocalConfigsEvent>(ModConst.SM_LOCAL_CONFIGS_EVENT);
-
-                if (monstData.monstType == MonstType.Common || monstData.monstType == MonstType.Elite)
-                {
-                    //add manashield
-                    if (CommonTool.Random(0.0f, 100.0f).IsBetween(0.0f, smConfigs.Calculate(MONST_EXPLODE_CHANCE * monstData.grade.value * gameLvl, smConfigs.Configs.AddSpecialMonsterRate).Parse<float>()))
-                    {
-                        _ExMonst.Add(e.data.createUnitSoleID);
-                    }
-                }
-            }
-        }
-
         public override void OnBattleUnitDie(UnitDie e)
         {
             base.OnBattleUnitDie(e);
 
             var dieUnit = e.unit;
-            if (_ExMonst.Contains(dieUnit.data.createUnitSoleID))
+            if (dieUnit.IsMonster())
             {
-                ModBattleEvent.SceneBattle.effect.CreateSync(EXPLODE_EFX, dieUnit.transform.position, 3f);
-                foreach (var cunit in dieUnit.FindNearObjects(3))
+                var gameLvl = g.data.dataWorld.data.gameLevel.Parse<int>();
+                var monstData = dieUnit?.data?.TryCast<UnitDataMonst>();
+                var smConfigs = EventHelper.GetEvent<SMLocalConfigsEvent>(ModConst.SM_LOCAL_CONFIGS_EVENT);
+
+                if (monstData.monstType == MonstType.Common || monstData.monstType == MonstType.Elite)
                 {
-                    if (cunit.IsWorldUnit())
+                    if (CommonTool.Random(0.0f, 100.0f).IsBetween(0.0f, smConfigs.Calculate(MONST_EXPLODE_CHANCE * monstData.grade.value * gameLvl, smConfigs.Configs.AddSpecialMonsterRate).Parse<float>()))
                     {
-                        MartialTool.HitDanage(dieUnit, cunit, new MartialTool.HitData(dieUnit, null, 0, 1, dieUnit.data.attack.baseValue));
+                        ModBattleEvent.SceneBattle.effect.CreateSync(EXPLODE_EFX, dieUnit.transform.position, 3f);
+                        foreach (var cunit in dieUnit.FindNearObjects(3))
+                        {
+                            if (cunit.IsWorldUnit())
+                            {
+                                MartialTool.HitDanage(dieUnit, cunit, new MartialTool.HitData(dieUnit, null, 0, 1, dieUnit.data.attack.baseValue));
+                            }
+                        }
                     }
                 }
             }
