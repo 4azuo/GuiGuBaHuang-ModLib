726b7aa9852228706947477701edcaec49bbf139
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2024-12-18 02:15:00 +0700

    4.0.2

--- 3161035078/ModProject/ModCode/ModMain/ModMain.cs
+++ 3161035078/ModProject/ModCode/ModMain/ModMain.cs
@@ -9,6 +9,7 @@ using System.Linq;
 
 namespace MOD_nE7UL2
 {
+    [ModOrder("_EventOrderIndex.json")]
     [Cache("nE7UL2", CacheType = CacheAttribute.CType.Global, WorkOn = CacheAttribute.WType.All, OrderIndex = 0)]
     public class ModMain : ModChild
     {
