832ac3d0ffbfc76226d75ecbc90614dff5a995ed
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2024-12-21 16:03:37 +0700

    4.0.4g

--- 3385996759/ModProject/ModCode/ModLib/Mod/ModUIEvent.cs
+++ 3385996759/ModProject/ModCode/ModLib/Mod/ModUIEvent.cs
@@ -2,7 +2,7 @@
 
 namespace ModLib.Mod
 {
-    [Cache("$UI$", OrderIndex = 10, CacheType = CacheAttribute.CType.Global)]
+    [Cache("$UI$", OrderIndex = 10, CacheType = CacheAttribute.CType.Global, WorkOn = CacheAttribute.WType.All)]
     public class ModUIEvent : ModEvent
     {
         public override void OnTimeUpdate()
