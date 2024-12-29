ec945218a3ca68ad38bfef031ec0fe89c27d2dd0
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2024-12-29 13:46:46 +0700

    Translator:1.0.0

---
+++ 3394439066/ModProject/ModCode/ModMain/ModMain.cs
@@ -0,0 +1,10 @@
+﻿using ModLib.Mod;
+using UnityEngine;
+
+namespace MOD_Xc8DI7
+{
+    [Cache("Xc8DI7", CacheType = CacheAttribute.CType.Global, OrderIndex = 0)]
+    public class ModMain : ModChild
+    {
+    }
+}
