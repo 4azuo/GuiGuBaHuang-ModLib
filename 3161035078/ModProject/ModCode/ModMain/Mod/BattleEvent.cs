5c46954ebbab2ac51ab8c04e54736dd0048722ab
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2025-01-07 03:31:10 +0700

    commit 7

--- 3161035078/ModProject/ModCode/ModMain/Mod/BattleEvent.cs
+++ 3161035078/ModProject/ModCode/ModMain/Mod/BattleEvent.cs
@@ -14,10 +14,12 @@ namespace MOD_nE7UL2.Mod
 
         public const int JOIN_RANGE = 6;
         public const float RANDOM_NPC_JOIN_RATE = 0.2f;
+        public const float SECT_NPC_JOIN_RATE = 2.0f;
+        public const float TOWN_GUARD_NPC_JOIN_RATE = 5.0f;
         public const int ENEMY_JOIN_DRAMA = 480110100;
         public const int FRIENDLY_JOIN_DRAMA = 480110200;
-        public const float SECT_NPC_JOIN_RATE = 1.0f;
-        public const float TOWN_GUARD_NPC_JOIN_RATE = 2.0f;
+        public const int SECT_MEMBER_JOIN_DRAMA = 480110300;
+        public const int TOWN_GUARD_JOIN_DRAMA = 480110400;
 
         public int PvPCount { get; set; } = 0;
 
@@ -41,32 +43,62 @@ namespace MOD_nE7UL2.Mod
             if (g.world.battle.data.isRealBattle && _aroundUnits != null)
             {
                 //enemy unit join battle
-                var enemyUnit = _aroundUnits.FirstOrDefault(x => IsEnemyUnit(x));
-                if (enemyUnit != null &&
-                    CommonTool.Random(0.00f, 100.00f).IsBetween(0.00f, RANDOM_NPC_JOIN_RATE))
+                if (CommonTool.Random(0.00f, 100.00f).IsBetween(0.00f, RANDOM_NPC_JOIN_RATE))
                 {
-                    _aroundUnits.Remove(enemyUnit);
-                    SceneType.battle.unit.CreateUnitHuman<UnitCtrlHumanNPC>(enemyUnit.data, UnitType.Alone);
-                    
-                    //var enemyUnitAllies = _aroundUnits.Where(x => enemyUnit.data.unitData.relationData.GetIntim(x) >= 350f);
-                    //foreach (var enemyUnitAllyUnit in enemyUnitAllies)
-                    //{
-                    //    _aroundUnits.Remove(enemyUnitAllyUnit);
-                    //    SceneType.battle.unit.CreateUnitHuman<UnitCtrlHumanNPC>(enemyUnitAllyUnit.data, UnitType.Monst);
-                    //}
+                    var enemyUnit = _aroundUnits.FirstOrDefault(x => IsEnemyUnit(x));
+                    if (enemyUnit != null)
+                    {
+                        _aroundUnits.Remove(enemyUnit);
+                        SceneType.battle.unit.CreateUnitHuman<UnitCtrlHumanNPC>(enemyUnit.data, UnitType.Alone);
 
-                    DramaTool.OpenDrama(ENEMY_JOIN_DRAMA, new DramaData() { unitLeft = enemyUnit, unitRight = g.world.playerUnit });
+                        //var enemyUnitAllies = _aroundUnits.Where(x => enemyUnit.data.unitData.relationData.GetIntim(x) >= 350f);
+                        //foreach (var enemyUnitAllyUnit in enemyUnitAllies)
+                        //{
+                        //    _aroundUnits.Remove(enemyUnitAllyUnit);
+                        //    SceneType.battle.unit.CreateUnitHuman<UnitCtrlHumanNPC>(enemyUnitAllyUnit.data, UnitType.Monst);
+                        //}
+
+                        DramaTool.OpenDrama(ENEMY_JOIN_DRAMA, new DramaData() { unitLeft = enemyUnit, unitRight = g.world.playerUnit });
+                    }
                 }
 
                 //friendly unit join battle
-                var friendlyUnit = _aroundUnits.FirstOrDefault(x => IsFriendlyUnit(x));
-                if (friendlyUnit != null &&
-                    CommonTool.Random(0.00f, 100.00f).IsBetween(0.00f, RANDOM_NPC_JOIN_RATE))
+                if (CommonTool.Random(0.00f, 100.00f).IsBetween(0.00f, RANDOM_NPC_JOIN_RATE))
                 {
-                    _aroundUnits.Remove(friendlyUnit);
-                    SceneType.battle.unit.CreateUnitHuman<UnitCtrlHumanNPC>(friendlyUnit.data, UnitType.PlayerNPC);
+                    var friendlyUnit = _aroundUnits.FirstOrDefault(x => IsFriendlyUnit(x));
+                    if (friendlyUnit != null)
+                    {
+                        _aroundUnits.Remove(friendlyUnit);
+                        SceneType.battle.unit.CreateUnitHuman<UnitCtrlHumanNPC>(friendlyUnit.data, UnitType.PlayerNPC);
 
-                    DramaTool.OpenDrama(FRIENDLY_JOIN_DRAMA, new DramaData() { unitLeft = friendlyUnit, unitRight = g.world.playerUnit });
+                        DramaTool.OpenDrama(FRIENDLY_JOIN_DRAMA, new DramaData() { unitLeft = friendlyUnit, unitRight = g.world.playerUnit });
+                    }
+                }
+
+                //sect member join battle
+                if (ModBattleEvent.School != null && CommonTool.Random(0.00f, 100.00f).IsBetween(0.00f, SECT_NPC_JOIN_RATE))
+                {
+                    var sectMember = _aroundUnits.FirstOrDefault(x => MapBuildPropertyEvent.IsSchoolMember(ModBattleEvent.School, x));
+                    if (sectMember != null)
+                    {
+                        _aroundUnits.Remove(sectMember);
+                        SceneType.battle.unit.CreateUnitHuman<UnitCtrlHumanNPC>(sectMember.data, IsEnemyUnit(sectMember) ? UnitType.Monst : UnitType.PlayerNPC);
+
+                        DramaTool.OpenDrama(SECT_MEMBER_JOIN_DRAMA, new DramaData() { unitLeft = sectMember, unitRight = null });
+                    }
+                }
+
+                //town guard join battle
+                if (ModBattleEvent.Town != null && CommonTool.Random(0.00f, 100.00f).IsBetween(0.00f, TOWN_GUARD_NPC_JOIN_RATE))
+                {
+                    var townguard = _aroundUnits.FirstOrDefault(x => MapBuildPropertyEvent.IsTownGuardian(ModBattleEvent.Town, x));
+                    if (townguard != null)
+                    {
+                        _aroundUnits.Remove(townguard);
+                        SceneType.battle.unit.CreateUnitHuman<UnitCtrlHumanNPC>(townguard.data, IsEnemyUnit(townguard) ? UnitType.Monst : UnitType.PlayerNPC);
+
+                        DramaTool.OpenDrama(TOWN_GUARD_JOIN_DRAMA, new DramaData() { unitLeft = townguard, unitRight = null });
+                    }
                 }
             }
         }
