85afed6a9a7ac5b7a437787fc66671d5431ea6a2
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2025-01-25 01:06:37 +0900

    4.5.14

--- 3161035078/ModProject/ModCode/ModMain/Mod/CustomRefineEvent.cs
+++ 3161035078/ModProject/ModCode/ModMain/Mod/CustomRefineEvent.cs
@@ -42,7 +42,7 @@ namespace MOD_nE7UL2.Mod
             base.OnMonthlyForEachWUnit(wunit);
             var location = wunit.GetUnitPos();
             var town = g.world.build.GetBuild<MapBuildTown>(location);
-            if (!wunit.IsPlayer() && town.IsSmallTown() && wunit.GetUnitMoney() > MapBuildPropertyEvent.GetTax(wunit, wunit.GetUnitPosAreaId()) * 3)
+            if (!wunit.IsPlayer() && town.IsSmallTown() && wunit.GetUnitMoney() > MapBuildPropertyEvent.GetTax(town, wunit) * 3)
             {
                 foreach (var item in GetRefinableItems(wunit))
                 {
