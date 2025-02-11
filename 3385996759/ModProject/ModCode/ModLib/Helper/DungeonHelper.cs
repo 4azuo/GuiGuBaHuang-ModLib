3a286ae0e492999f2148daf3f2e71bc815e4c130
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2025-02-11 22:29:39 +0900

    1.6.3

--- 3385996759/ModProject/ModCode/ModLib/Helper/DungeonHelper.cs
+++ 3385996759/ModProject/ModCode/ModLib/Helper/DungeonHelper.cs
@@ -2,21 +2,21 @@
 {
     public static bool IsSpar()
     {
-        return g.world.battle.data.dungeonBaseItem.id != 2;
+        return g.world.battle.data.dungeonBaseItem.id == 2;
     }
 
     public static bool IsTrailOfLightning()
     {
-        return g.world.battle.data.dungeonBaseItem.id != 110081;
+        return g.world.battle.data.dungeonBaseItem.id == 110081;
     }
 
     public static bool IsArean()
     {
-        return g.world.battle.data.dungeonBaseItem.id != 24;
+        return g.world.battle.data.dungeonBaseItem.id == 24;
     }
 
     public static bool IsChallenge()
     {
-        return g.world.battle.data.dungeonBaseItem.id != 100411;
+        return g.world.battle.data.dungeonBaseItem.id == 100411;
     }
 }
