9ee5db56059067d5cfc655b197958fc828b05bdd
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2025-02-18 23:37:15 +0900

    1.6.8

---
+++ 3385996759/ModProject/ModCode/ModLib/Helper/FileHelper.cs
@@ -0,0 +1,19 @@
+﻿using ModLib.Object;
+using Newtonsoft.Json;
+using System.IO;
+
+public static class FileHelper
+{
+    public static bool IsReadable<T>(string filePath) where T : CachableObject
+    {
+        try
+        {
+            JsonConvert.DeserializeObject<T>(File.ReadAllText(filePath));
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
