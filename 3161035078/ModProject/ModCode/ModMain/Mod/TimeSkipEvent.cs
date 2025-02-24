04b4a853baedaff6b2c598da9ab3e5135358e0a9
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2025-02-25 01:31:28 +0900

    4.6.12: build 2

--- 3161035078/ModProject/ModCode/ModMain/Mod/TimeSkipEvent.cs
+++ 3161035078/ModProject/ModCode/ModMain/Mod/TimeSkipEvent.cs
@@ -10,6 +10,7 @@ namespace MOD_nE7UL2.Mod
     [Cache(ModConst.TIME_SKIP_EVENT)]
     public class TimeSkipEvent : ModEvent
     {
+        private static UICover<UIMapMain> uiMain;
         private static int skip2Month = -1;
 
         public override void OnOpenUIEnd(OpenUIEnd e)
@@ -17,38 +18,39 @@ namespace MOD_nE7UL2.Mod
             base.OnOpenUIEnd(e);
             if (e.uiType.uiName == UIType.MapMain.uiName)
             {
-                var ui = new UICover<UIMapMain>(e.ui);
+                uiMain = new UICover<UIMapMain>(e.ui);
                 {
-                    var btnFateFeature = ui.UI.playerInfo.btnFateFeature ?? ui.UI.playerInfo.btnFateFeature_En;
-                    ui.AddButton(btnFateFeature.transform.position.x + 3f, btnFateFeature.transform.position.y, () =>
+                    var btnFateFeature = uiMain.UI.playerInfo.btnFateFeature ?? uiMain.UI.playerInfo.btnFateFeature_En;
+                    uiMain.AddButton(btnFateFeature.transform.position.x + 3f, btnFateFeature.transform.position.y, () =>
                     {
-                        g.ui.MsgBox(GameTool.LS("other500020022"), "Do you really wanna skip 1 year?", MsgBoxButtonEnum.YesNo, () =>
+                        g.ui.MsgBox(GameTool.LS("other500020022"), string.Format(GameTool.LS("other500020029"), 1), MsgBoxButtonEnum.YesNo, () =>
                         {
                             SkipTime(12);
                         });
-                    }, "Skip 1 year").Size(140, 40);
-                    ui.AddButton(btnFateFeature.transform.position.x + 5f, btnFateFeature.transform.position.y, () =>
-                    {
-                        g.ui.MsgBox(GameTool.LS("other500020022"), "Do you really wanna skip 10 year?", MsgBoxButtonEnum.YesNo, () =>
-                        {
-                            SkipTime(120);
-                        });
-                    }, "Skip 10 year").Size(140, 40);
+                    }, string.Format(GameTool.LS("other500020030"), 1)).Size(140, 40);
+                    //uiMain.AddButton(btnFateFeature.transform.position.x + 5f, btnFateFeature.transform.position.y, () =>
+                    //{
+                    //    g.ui.MsgBox(GameTool.LS("other500020022"), string.Format(GameTool.LS("other500020029"), 10), MsgBoxButtonEnum.YesNo, () =>
+                    //    {
+                    //        SkipTime(120);
+                    //    });
+                    //}, string.Format(GameTool.LS("other500020030"), 10)).Size(140, 40);
                 }
             }
         }
 
         public static void SkipTime(int month)
         {
-            skip2Month = (GameHelper.GetGameTotalMonth() + month) * 30;
+            skip2Month = GameHelper.GetGameTotalMonth() + month;
         }
 
         public override void OnTimeUpdate1s()
         {
             base.OnTimeUpdate1s();
-            if (GameHelper.GetGameTotalMonth() < skip2Month && !g.world.run.isRunning && g.world.run.IsCanRun())
+            if (g.ui.HasUI(UIType.MapMain) && uiMain != null &&
+                GameHelper.GetGameTotalMonth() < skip2Month && !g.world.run.isRunning && g.world.run.IsCanRun())
             {
-                //g.world.run.RunWorld(g.world.run.on);
+                uiMain.UI.playerInfo.btnNextMonth.onClick.Invoke();
             }
         }
     }
