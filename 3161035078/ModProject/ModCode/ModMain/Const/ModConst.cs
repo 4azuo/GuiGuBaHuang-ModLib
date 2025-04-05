39a29007318e03fb822695c3a9a344d83a87fca4
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2025-04-05 23:49:33 +0900

    4.7.6: build 2

--- 3161035078/ModProject/ModCode/ModMain/Const/ModConst.cs
+++ 3161035078/ModProject/ModCode/ModMain/Const/ModConst.cs
@@ -2,8 +2,8 @@
 {
     public static class ModConst
     {
-        public const string TAOIST_VERSION = "4.7.4";
-        public const string MODLIB_REQUIRED_VERSION = "1.7.2";
+        public const string TAOIST_VERSION = "4.7.6";
+        public const string MODLIB_REQUIRED_VERSION = "1.7.3";
 
         //Events
         public const string DEBUG_EVENT = "DEBUG";
--- 3161035078/ModProject/ModCode/ModMain/Mod/RealMarketEvent3.cs
+++ 3161035078/ModProject/ModCode/ModMain/Mod/RealMarketEvent3.cs
@@ -4,7 +4,8 @@ using MOD_nE7UL2.Const;
 using ModLib.Mod;
 using UnityEngine;
 using ModLib.Object;
-using Boo.Lang;
+using System.Collections.Generic;
+using System;
 
 namespace MOD_nE7UL2.Mod
 {
@@ -17,11 +18,35 @@ namespace MOD_nE7UL2.Mod
         public static RealMarketEvent3 Instance { get; set; }
         public static bool isShowNPCList = false;
 
-        public List<string> NPCinMarket = new List<string>();
+        public Dictionary<string, List<Tuple<string, int>>> NPCinMarket = new Dictionary<string, List<Tuple<string, int>>>();
+
+        public const float JOIN_MARKET_RATE = 30f;
+        public const float SELL_RATE = 30f;
 
         public override void OnMonthly()
         {
             base.OnMonthly();
+            NPCinMarket.Clear();
+            foreach (var town in g.world.build.GetBuilds<MapBuildTown>())
+            {
+                var market = town.GetBuildSub<MapBuildTownMarket>();
+                if (market != null)
+                {
+                    var wunits = g.world.unit.GetUnitExact(town.GetOrigiPoint(), 4).ToArray();
+                    foreach (var wunit in wunits)
+                    {
+                        if (CommonTool.Random(0f, 100f).IsBetween(0f, JOIN_MARKET_RATE))
+                        {
+                            var props = wunit.GetUnequippedProps()
+                                .Where(x => x.propsInfoBase.sale > 0 && CommonTool.Random(0f, 100f).IsBetween(0f, SELL_RATE))
+                                .Select(x => new Tuple<string, int>(x.soleID, CommonTool.Random(1, x.propsCount)))
+                                .ToList();
+                            if (props.Count > 0)
+                                NPCinMarket.Add(wunit.GetUnitId(), props);
+                        }
+                    }
+                }
+            }
         }
 
         public override void OnOpenUIEnd(OpenUIEnd e)
