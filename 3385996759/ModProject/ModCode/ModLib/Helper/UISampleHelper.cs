d691bf8954ddb1ff262acadad7dbaddc87f8a4cb
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2025-04-26 14:02:43 +0900

    1.8.1

--- 3385996759/ModProject/ModCode/ModLib/Helper/UISampleHelper.cs
+++ 3385996759/ModProject/ModCode/ModLib/Helper/UISampleHelper.cs
@@ -15,39 +15,42 @@ public static class UISampleHelper
 
     public static void LoadUISampples()
     {
-        // Create Canvas
-        Il2CppSystem.Type canvasType = Il2CppSystem.Type.GetType("UnityEngine.Canvas, UnityEngine");
-        var canvasGO = new GameObject(PERSISTENT_CANVAS_NAME, new Il2CppSystem.Type[] { canvasType });
-        var canvas = canvasGO.GetComponent<Canvas>();
-
-        // Prevent Canvas from being destroyed across scenes
-        Object.DontDestroyOnLoad(canvasGO);
-
-        // Hide this canvas
-        canvas.gameObject.SetActive(false);
-
-        using (var s = new UISample<UIGameSetting>(UIType.GameSetting))
+        g.ui.Init((Il2CppSystem.Action)(() =>
         {
-            ButtonSample = s.ui.btnSystemOK.Create(canvasGO.transform);
-            //Object.DontDestroyOnLoad(ButtonSample);
-            SelectSample = s.ui.tglLanguage.Create(canvasGO.transform);
-            //Object.DontDestroyOnLoad(SelectSample);
-            SliderSample = s.ui.sliSoundMain.Create(canvasGO.transform);
-            //Object.DontDestroyOnLoad(SliderSample);
-            TextSample = s.ui.textSystemOK.Create(canvasGO.transform);
-            //Object.DontDestroyOnLoad(TextSample);
-            ToggleSample = s.ui.tglWindow.Create(canvasGO.transform);
-            //Object.DontDestroyOnLoad(ToggleSample);
-        }
-        using (var s = new UISample<UIModWorkshopUpload>(UIType.ModWorkshopUpload))
-        {
-            InputSample = s.ui.iptDesc.Create(canvasGO.transform);
-            //Object.DontDestroyOnLoad(InputSample);
-        }
-        using (var s = new UISample<UIPropInfo>(UIType.PropInfo))
-        {
-            ImageSample = s.ui.imgIcon.Create(canvasGO.transform);
-            //Object.DontDestroyOnLoad(ImageSample);
-        }
+            // Create Canvas
+            Il2CppSystem.Type canvasType = Il2CppSystem.Type.GetType("UnityEngine.Canvas, UnityEngine");
+            var canvasGO = new GameObject(PERSISTENT_CANVAS_NAME, new Il2CppSystem.Type[] { canvasType });
+            var canvas = canvasGO.GetComponent<Canvas>();
+
+            // Prevent Canvas from being destroyed across scenes
+            Object.DontDestroyOnLoad(canvasGO);
+
+            // Hide this canvas
+            canvas.gameObject.SetActive(false);
+
+            using (var s = new UISample<UIGameSetting>(UIType.GameSetting))
+            {
+                ButtonSample = s.ui.btnSystemOK.Create(canvasGO.transform);
+                //Object.DontDestroyOnLoad(ButtonSample);
+                SelectSample = s.ui.tglLanguage.Create(canvasGO.transform);
+                //Object.DontDestroyOnLoad(SelectSample);
+                SliderSample = s.ui.sliSoundMain.Create(canvasGO.transform);
+                //Object.DontDestroyOnLoad(SliderSample);
+                TextSample = s.ui.textSystemOK.Create(canvasGO.transform);
+                //Object.DontDestroyOnLoad(TextSample);
+                ToggleSample = s.ui.tglWindow.Create(canvasGO.transform);
+                //Object.DontDestroyOnLoad(ToggleSample);
+            }
+            using (var s = new UISample<UIModWorkshopUpload>(UIType.ModWorkshopUpload))
+            {
+                InputSample = s.ui.iptDesc.Create(canvasGO.transform);
+                //Object.DontDestroyOnLoad(InputSample);
+            }
+            using (var s = new UISample<UIPropInfo>(UIType.PropInfo))
+            {
+                ImageSample = s.ui.imgIcon.Create(canvasGO.transform);
+                //Object.DontDestroyOnLoad(ImageSample);
+            }
+        }));
     }
 }
