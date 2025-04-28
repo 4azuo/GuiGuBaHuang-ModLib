b113c80d39987172286fdbcee15698b1b593cad2
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2025-04-29 00:39:48 +0900

    2.0.2

--- 3385996759/ModProject/ModCode/ModLib/Helper/UISampleHelper.cs
+++ 3385996759/ModProject/ModCode/ModLib/Helper/UISampleHelper.cs
@@ -28,37 +28,24 @@ public static class UISampleHelper
             // Hide this canvas
             canvas.gameObject.SetActive(false);
 
-            //var UIGameSetting = g.ui.OpenUISafe<UIGameSetting>(UIType.GameSetting);
             using (var s = new UISample<UIGameSetting>(UIType.GameSetting))
             {
                 ButtonSample = s.ui.btnSystemOK.Create(canvasGO.transform);
-                //Object.DontDestroyOnLoad(ButtonSample);
                 SelectSample = s.ui.tglLanguage.Create(canvasGO.transform);
-                //Object.DontDestroyOnLoad(SelectSample);
                 SliderSample = s.ui.sliSoundMain.Create(canvasGO.transform);
-                //Object.DontDestroyOnLoad(SliderSample);
                 TextSample = s.ui.textSystemOK.Create(canvasGO.transform);
-                //Object.DontDestroyOnLoad(TextSample);
                 ToggleSample = s.ui.tglWindow.Create(canvasGO.transform);
-                //Object.DontDestroyOnLoad(ToggleSample);
             }
-            //g.ui.CloseUI(UIGameSetting);
 
-            //var UIModWorkshopUpload = g.ui.OpenUISafe<UIModWorkshopUpload>(UIType.ModWorkshopUpload);
             using (var s = new UISample<UIModWorkshopUpload>(UIType.ModWorkshopUpload))
             {
                 InputSample = s.ui.iptDesc.Create(canvasGO.transform);
-                //Object.DontDestroyOnLoad(InputSample);
             }
-            //g.ui.CloseUI(UIModWorkshopUpload);
 
-            //var UIPropInfo = g.ui.OpenUISafe<UIPropInfo>(UIType.PropInfo);
             using (var s = new UISample<UIPropInfo>(UIType.PropInfo))
             {
                 ImageSample = s.ui.imgIcon.Create(canvasGO.transform);
-                //Object.DontDestroyOnLoad(ImageSample);
             }
-            //g.ui.CloseUI(UIPropInfo);
         }));
     }
 }
