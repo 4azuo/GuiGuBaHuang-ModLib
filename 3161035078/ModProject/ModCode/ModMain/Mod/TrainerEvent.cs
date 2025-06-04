ee4bec4eeecec9b14e6881775b984843a8462430
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2025-06-05 00:45:53 +0900

    5.1.8

--- 3161035078/ModProject/ModCode/ModMain/Mod/TrainerEvent.cs
+++ 3161035078/ModProject/ModCode/ModMain/Mod/TrainerEvent.cs
@@ -150,35 +150,35 @@ namespace MOD_nE7UL2.Mod
                 uiTrainer.AddText(col - 1, row++, GameTool.LS("trainer038")).Format(null, 17, FontStyle.Italic).Align(TextAnchor.MiddleCenter);
                 uiTrainer.AddText(col, row, "HP: {0}/{1}").Align(TextAnchor.MiddleLeft).Format(Color.black, 15).SetWork(new UIItemWork
                 {
-                    Formatter = (x) => new object[] { player.GetDynProperty(UnitDynPropertyEnum.Hp).value.ToString("#,##0"), player.GetDynProperty(UnitDynPropertyEnum.HpMax).value.ToString("#,##0") },
+                    Formatter = (x) => new object[] { player.GetDynProperty(UnitDynPropertyEnum.Hp).value.ToString(ModConst.FORMAT_NUMBER), player.GetDynProperty(UnitDynPropertyEnum.HpMax).value.ToString(ModConst.FORMAT_NUMBER) },
                 });
                 uiTrainer.AddText(col - 3, row++, "{0} :Money").Align(TextAnchor.MiddleRight).Format(Color.black, 15).SetWork(new UIItemWork
                 {
-                    Formatter = (x) => new object[] { player.GetUnitMoney().ToString("#,##0") },
+                    Formatter = (x) => new object[] { player.GetUnitMoney().ToString(ModConst.FORMAT_NUMBER) },
                 });
                 uiTrainer.AddText(col, row, "MP: {0}/{1}").Align(TextAnchor.MiddleLeft).Format(Color.black, 15).SetWork(new UIItemWork
                 {
-                    Formatter = (x) => new object[] { player.GetDynProperty(UnitDynPropertyEnum.Mp).value.ToString("#,##0"), player.GetDynProperty(UnitDynPropertyEnum.MpMax).value.ToString("#,##0") },
+                    Formatter = (x) => new object[] { player.GetDynProperty(UnitDynPropertyEnum.Mp).value.ToString(ModConst.FORMAT_NUMBER), player.GetDynProperty(UnitDynPropertyEnum.MpMax).value.ToString(ModConst.FORMAT_NUMBER) },
                 });
                 uiTrainer.AddText(col - 3, row++, "{0} :Degree").Align(TextAnchor.MiddleRight).Format(Color.black, 15).SetWork(new UIItemWork
                 {
-                    Formatter = (x) => new object[] { player.GetUnitMayorDegree().ToString("#,##0") },
+                    Formatter = (x) => new object[] { player.GetUnitMayorDegree().ToString(ModConst.FORMAT_NUMBER) },
                 });
                 uiTrainer.AddText(col, row, "SP: {0}/{1}").Align(TextAnchor.MiddleLeft).Format(Color.black, 15).SetWork(new UIItemWork
                 {
-                    Formatter = (x) => new object[] { player.GetDynProperty(UnitDynPropertyEnum.Sp).value.ToString("#,##0"), player.GetDynProperty(UnitDynPropertyEnum.SpMax).value.ToString("#,##0") },
+                    Formatter = (x) => new object[] { player.GetDynProperty(UnitDynPropertyEnum.Sp).value.ToString(ModConst.FORMAT_NUMBER), player.GetDynProperty(UnitDynPropertyEnum.SpMax).value.ToString(ModConst.FORMAT_NUMBER) },
                 });
                 uiTrainer.AddText(col - 3, row++, "{0} :Contribution").Align(TextAnchor.MiddleRight).Format(Color.black, 15).SetWork(new UIItemWork
                 {
-                    Formatter = (x) => new object[] { player.GetUnitContribution().ToString("#,##0") },
+                    Formatter = (x) => new object[] { player.GetUnitContribution().ToString(ModConst.FORMAT_NUMBER) },
                 });
                 uiTrainer.AddText(col, row, "Atk: {0}").Align(TextAnchor.MiddleLeft).Format(Color.black, 15).SetWork(new UIItemWork
                 {
-                    Formatter = (x) => new object[] { player.GetDynProperty(UnitDynPropertyEnum.Attack).value.ToString("#,##0") },
+                    Formatter = (x) => new object[] { player.GetDynProperty(UnitDynPropertyEnum.Attack).value.ToString(ModConst.FORMAT_NUMBER) },
                 });
                 uiTrainer.AddText(col - 3, row++, "{0} :Def").Align(TextAnchor.MiddleRight).Format(Color.black, 15).SetWork(new UIItemWork
                 {
-                    Formatter = (x) => new object[] { player.GetDynProperty(UnitDynPropertyEnum.Defense).value.ToString("#,##0") },
+                    Formatter = (x) => new object[] { player.GetDynProperty(UnitDynPropertyEnum.Defense).value.ToString(ModConst.FORMAT_NUMBER) },
                 });
                 uiTrainer.AddText(col, row, "Mood: {0}/{1}").Align(TextAnchor.MiddleLeft).Format(Color.black, 15).SetWork(new UIItemWork
                 {
@@ -194,7 +194,7 @@ namespace MOD_nE7UL2.Mod
                 });
                 uiTrainer.AddText(col - 3, row++, "{0}/{1} :Life").Align(TextAnchor.MiddleRight).Format(Color.black, 15).SetWork(new UIItemWork
                 {
-                    Formatter = (x) => new object[] { player.GetDynProperty(UnitDynPropertyEnum.Age).value.ToString("#,##0"), player.GetDynProperty(UnitDynPropertyEnum.Life).value.ToString("#,##0") },
+                    Formatter = (x) => new object[] { player.GetDynProperty(UnitDynPropertyEnum.Age).value.ToString(ModConst.FORMAT_NUMBER), player.GetDynProperty(UnitDynPropertyEnum.Life).value.ToString(ModConst.FORMAT_NUMBER) },
                 });
                 uiTrainer.AddText(col, row, "Travel Speed: {0}").Align(TextAnchor.MiddleLeft).Format(Color.black, 15).SetWork(new UIItemWork
                 {
@@ -210,7 +210,7 @@ namespace MOD_nE7UL2.Mod
                 });
                 uiTrainer.AddText(col - 3, row++, "({1}) {0} :Ability").Align(TextAnchor.MiddleRight).Format(Color.black, 15).SetWork(new UIItemWork
                 {
-                    Formatter = (x) => new object[] { player.GetDynProperty(UnitDynPropertyEnum.AbilityExp).value.ToString("#,##0"), player.GetDynProperty(UnitDynPropertyEnum.AbilityPoint).value.ToString("#,##0") },
+                    Formatter = (x) => new object[] { player.GetDynProperty(UnitDynPropertyEnum.AbilityExp).value.ToString(ModConst.FORMAT_NUMBER), player.GetDynProperty(UnitDynPropertyEnum.AbilityPoint).value.ToString(ModConst.FORMAT_NUMBER) },
                 });
                 uiTrainer.AddText(col, row, "Luck: {0}").Align(TextAnchor.MiddleLeft).Format(Color.black, 15).SetWork(new UIItemWork
                 {
@@ -222,11 +222,11 @@ namespace MOD_nE7UL2.Mod
                 });
                 uiTrainer.AddText(col, row, "Exp: {0}/{1}").Align(TextAnchor.MiddleLeft).Format(Color.black, 15).SetWork(new UIItemWork
                 {
-                    Formatter = (x) => new object[] { player.GetExp().ToString("#,##0"), player.GetMaxExpCurrentPhase().ToString("#,##0") },
+                    Formatter = (x) => new object[] { player.GetExp().ToString(ModConst.FORMAT_NUMBER), player.GetMaxExpCurrentPhase().ToString(ModConst.FORMAT_NUMBER) },
                 });
                 uiTrainer.AddText(col - 3, row++, "{0} :Reputation").Align(TextAnchor.MiddleRight).Format(Color.black, 15).SetWork(new UIItemWork
                 {
-                    Formatter = (x) => new object[] { player.GetDynProperty(UnitDynPropertyEnum.Reputation).value.ToString("#,##0") },
+                    Formatter = (x) => new object[] { player.GetDynProperty(UnitDynPropertyEnum.Reputation).value.ToString(ModConst.FORMAT_NUMBER) },
                 });
                 uiTrainer.AddText(col - 1, row++, "Grade: {0} {1} ― {2}").Align(TextAnchor.MiddleCenter).Format(Color.black, 15).SetWork(new UIItemWork
                 {
