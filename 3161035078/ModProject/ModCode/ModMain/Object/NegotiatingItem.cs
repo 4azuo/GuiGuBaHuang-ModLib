1811ed74a9e339cc891d7eb31ff2b94bef228d5d
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2025-06-08 00:31:37 +0700

    5.1.8c

---
+++ 3161035078/ModProject/ModCode/ModMain/Object/NegotiatingItem.cs
@@ -0,0 +1,10 @@
+﻿namespace MOD_nE7UL2.Object
+{
+    public class NegotiatingItem
+    {
+        public string TargetPropSoleId { get; set; }
+        public string BuyerId { get; set; }
+        public string NegotiatingPropSoleId { get; set; }
+        public int Count { get; set; }
+    }
+}
