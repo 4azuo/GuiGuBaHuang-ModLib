cb9398078423055df962c3a3d853f94a56aa43af
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2025-06-27 23:24:02 +0900

    2.1.3

---
+++ 3385996759/ModProject/ModCode/ModLib/Helper/DramaHelper.cs
@@ -0,0 +1,30 @@
+﻿using Il2CppSystem;
+using Il2CppSystem.Collections.Generic;
+
+public static partial class DramaHelper
+{
+    public static void OpenDrama(int dramaID, string text, Dictionary<int, string> optIds, List<int> hideOptIds, Action<ConfDramaOptionsItem> optClickCall)
+    {
+        DramaTool.OpenDrama(dramaID, new DramaData
+        {
+            dialogueText = { [dramaID] = text },
+            dialogueOptions = optIds,
+            hideDialogueOptions = hideOptIds,
+            onOptionsClickCall = optClickCall,
+        });
+    }
+
+    public static void OpenDrama(int dramaID, string text, Dictionary<int, string> optIds, List<int> hideOptIds, Action<ConfDramaOptionsItem> optClickCall,
+        WorldUnitBase wunitLeft, WorldUnitBase wunitRight)
+    {
+        DramaTool.OpenDrama(dramaID, new DramaData
+        {
+            dialogueText = { [dramaID] = text },
+            dialogueOptions = optIds,
+            hideDialogueOptions = hideOptIds,
+            onOptionsClickCall = optClickCall,
+            unitLeft = wunitLeft,
+            unitRight = wunitRight,
+        });
+    }
+}
