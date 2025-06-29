b33558e213b34a44297aea7ad545b83e7e6915ea
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2025-06-29 23:30:08 +0900

    2.1.7

--- 3385996759/ModProject/ModCode/ModLib/Helper/DramaHelper.cs
+++ 3385996759/ModProject/ModCode/ModLib/Helper/DramaHelper.cs
@@ -5,50 +5,46 @@ using System.Linq;
 
 public static class DramaHelper
 {
-    public static void OpenDrama1(string text, Dictionary<int, string> optIds, List<int> hideOptIds, Action<ConfDramaOptionsItem> optClickCall)
+    private const int DEFAULT_OPT_PREFIX_ID = 999790000;
+    private static int[] DEFAULT_OPTS = new int[] { 999790000, 999790001, 999790002, 999790003, 999790004, 999790005, 999790006, 999790007, 999790008, 999790009 };
+
+    public static void OpenDrama1(string text, List<string> optMsgs, Action<ConfDramaOptionsItem> optClickCall)
     {
+        var opts = optMsgs.Select((x, i) => new { Index = i, Value = x }).ToArray();
         DramaTool.OpenDrama(ModLibConst.DEFAULT_TYPE1_DRAMA_ID, new DramaData
         {
             dialogueText = { [ModLibConst.DEFAULT_TYPE1_DRAMA_ID] = text },
-            dialogueOptions = (optIds ?? GetDefaultOpt())?.ToIl2CppDictionary(x => x.Key, x => x.Value),
-            hideDialogueOptions = hideOptIds?.ToIl2CppList(),
+            dialogueOptions = opts.ToIl2CppDictionary(x => DEFAULT_OPT_PREFIX_ID + x.Index, x => x.Value),
+            hideDialogueOptions = DEFAULT_OPTS.Except(opts.Select((x, i) => i)).ToIl2CppList(),
             onOptionsClickCall = optClickCall,
         });
     }
 
-    public static void OpenDrama1(string text, Dictionary<int, string> optIds, List<int> hideOptIds, Action<ConfDramaOptionsItem> optClickCall,
+    public static void OpenDrama1(string text, List<string> optMsgs, Action<ConfDramaOptionsItem> optClickCall,
         WorldUnitBase wunitLeft, WorldUnitBase wunitRight)
     {
+        var opts = optMsgs.Select((x, i) => new { Index = i, Value = x }).ToArray();
         DramaTool.OpenDrama(ModLibConst.DEFAULT_TYPE1_DRAMA_ID, new DramaData
         {
             dialogueText = { [ModLibConst.DEFAULT_TYPE1_DRAMA_ID] = text },
-            dialogueOptions = (optIds ?? GetDefaultOpt())?.ToIl2CppDictionary(x => x.Key, x => x.Value),
-            hideDialogueOptions = hideOptIds?.ToIl2CppList(),
+            dialogueOptions = opts.ToIl2CppDictionary(x => DEFAULT_OPT_PREFIX_ID + x.Index, x => x.Value),
+            hideDialogueOptions = DEFAULT_OPTS.Except(opts.Select((x, i) => i)).ToIl2CppList(),
             onOptionsClickCall = optClickCall,
             unitLeft = wunitLeft,
             unitRight = wunitRight,
         });
     }
 
-    public static void OpenDrama2(string text, Dictionary<int, string> optIds, List<int> hideOptIds, Action<ConfDramaOptionsItem> optClickCall, string background)
+    public static void OpenDrama2(string text, List<string> optMsgs, Action<ConfDramaOptionsItem> optClickCall, string background)
     {
+        var opts = optMsgs.Select((x, i) => new { Index = i, Value = x }).ToArray();
         DramaTool.OpenDrama(ModLibConst.DEFAULT_TYPE2_DRAMA_ID, new DramaData
         {
             dialogueText = { [ModLibConst.DEFAULT_TYPE2_DRAMA_ID] = text },
-            dialogueOptions = (optIds ?? GetDefaultOpt())?.ToIl2CppDictionary(x => x.Key, x => x.Value),
-            hideDialogueOptions = hideOptIds?.ToIl2CppList(),
+            dialogueOptions = opts.ToIl2CppDictionary(x => DEFAULT_OPT_PREFIX_ID + x.Index, x => x.Value),
+            hideDialogueOptions = DEFAULT_OPTS.Except(opts.Select((x, i) => i)).ToIl2CppList(),
             onOptionsClickCall = optClickCall,
             backgroud = { [ModLibConst.DEFAULT_TYPE2_DRAMA_ID] = background },
         });
-    }
-
-    public static Dictionary<int, string> GetDefaultOpt()
-    {
-        return new Tuple<int, string>[] { new Tuple<int, string>(999890005, GameTool.LS("libtxt999990005")) }.ToDictionary(x => x.Item1, x => x.Item2);
-    }
-
-    public static Dictionary<int, string> CreateOpts(params string[] opts)
-    {
-        return opts.Select((x, i) => new { Index = i, Value = x }).ToDictionary(x => x.Index, x => x.Value);
     }
 }
