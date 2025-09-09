a5d085b899fda9b66d8a51b1005c9375c576ffa5
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2025-09-09 21:30:40 +0700

    5.4.5

--- 3161035078/ModProject/ModCode/ModMain/Const/ModConst.cs
+++ 3161035078/ModProject/ModCode/ModMain/Const/ModConst.cs
@@ -2,8 +2,8 @@
 {
     public static class ModConst
     {
-        public const string TAOIST_VERSION = "5.4.4";
-        public const string MODLIB_REQUIRED_VERSION = "2.1.18";
+        public const string TAOIST_VERSION = "5.4.5";
+        public const string MODLIB_REQUIRED_VERSION = "2.1.19";
 
         //Events
         public const string DEBUG_EVENT = "DEBUG";
