aaed7c69a586373fb773b0cceea1e8996a694e49
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2025-10-19 17:14:52 +0900

    2.2.7

--- 3385996759/ModProject/ModCode/ModLib/Object/ModConfigs.cs
+++ 3385996759/ModProject/ModCode/ModLib/Object/ModConfigs.cs
@@ -5,6 +5,6 @@ namespace ModLib.Object
     //you have to set default value for new ones
     public class ModConfigs
     {
-        public DebugModeEnum DebugMode { get; set; } = DebugModeEnum.Fine;
+        public DebugModeEnum DebugMode { get; set; } = DebugModeEnum.Info;
     }
 }
