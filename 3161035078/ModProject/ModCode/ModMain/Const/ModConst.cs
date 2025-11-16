b8a97b89158d6c4b21d8545861efac5989503ab0
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2025-11-16 17:34:00 +0900

    add copilot instructions

--- 3161035078/ModProject/ModCode/ModMain/Const/ModConst.cs
+++ 3161035078/ModProject/ModCode/ModMain/Const/ModConst.cs
@@ -2,7 +2,7 @@
 {
     public static class ModConst
     {
-        public const string TAOIST_VERSION = "5.5.26";
+        public const string TAOIST_VERSION = "5.5.27";
         public const string MODLIB_REQUIRED_VERSION = "2.2.13";
 
         //Events
