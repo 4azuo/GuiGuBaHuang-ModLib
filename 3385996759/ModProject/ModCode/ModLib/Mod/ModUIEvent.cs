57b372bad23e2cd019d861ff117badc546d7f423
Huỳnh Chí Phong <11677054+4azuo@users.noreply.github.com>
2025-12-29 11:57:47 +0700

    2.2.24

--- 3385996759/ModProject/ModCode/ModLib/Mod/ModUIEvent.cs
+++ 3385996759/ModProject/ModCode/ModLib/Mod/ModUIEvent.cs
@@ -13,5 +13,26 @@ namespace ModLib.Mod
             base.OnTimeUpdate100ms();
             UIHelper.UpdateAllUI(x => x.IsAutoUpdate);
         }
+
+        public override void OnMonoUpdate()
+        {
+            base.OnMonoUpdate();
+            if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.P))
+            {
+                DebugHelper.WriteLine("==================");
+                DebugHelper.WriteLine(g.res.abPath);
+                DebugHelper.WriteLine("===== allRes =====");
+                foreach (var r in g.res.allRes)
+                {
+                    DebugHelper.WriteLine($"{r.key}");
+                }
+                DebugHelper.WriteLine("===== allAB =====");
+                foreach (var r in g.res.allAB)
+                {
+                    DebugHelper.WriteLine($"{r.key}");
+                }
+                DebugHelper.Save();
+            }
+        }
     }
 }
